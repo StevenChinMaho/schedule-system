6609eb8e8806e31abfb7b29f32408329ff6a12e4
StevenChinMaho <503steven10@gmail.com>
2025-10-02 17:34:50 +0800

    SQL SELECT TEST

--- sendForm.php
+++ sendForm.php
@@ -1,29 +1,57 @@
-<?php
-    require_once("includes/config.php");
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SendForm</title>
+</head>
+<body>
+    <a href="index.html">回主頁</a><br>
+        <?php
+            require_once("includes/config.php");
 
-    if ( empty($_POST['name']) || empty($_POST['age']) )
-    {
-        echo "你少填了啦 阿齁<br>";
-        echo "<a href=\"index.html\">點我回去</a>";
-        exit();
-    }
-    else
-    {
-        echo "你好啊 " . $_POST['name'] . "，聽說你 " . $_POST['age'] . " 歲！<br>";
+            if ( empty($_POST['name']) || empty($_POST['age']) )
+            {
+                echo "你少填了啦 阿齁<br>";
+                echo "<a href=\"index.html\">點我回去</a>";
+                exit();
+            }
+            else
+            {
+                echo "你好啊 " . $_POST['name'] . "，聽說你 " . $_POST['age'] . " 歲！<br>";
 
-        $name = $_POST['name'];
-        $age = (int)$_POST['age'];
+                $name = $_POST['name'];
+                $age = (int)$_POST['age'];
 
-        // $sql = sprintf( "INSERT INTO test( name, age ) VALUES ( '%s', %s )", $name, $age );
+                $stmt = $pdo->prepare("INSERT INTO test( name, age ) VALUES ( ?, ? )");
+                $stmt->execute([$name, $age]);
+                echo "資料插入完成<br>";
+        ?>
+    <a href="tamamitsune.html">間隔用</a><br>
+    <hr>
+    <table>
+        <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>名字</th>
+                    <th>歲數</th>
+                </tr>
+        </thead>
+        <tbody>
+        <?php
+                $sql = "SELECT * FROM test";
+                $stmt = $pdo->query($sql);
+                $tests = $stmt->fetchAll();
 
-        $stmt = $pdo->prepare("INSERT INTO test( name, age ) VALUES ( ?, ? )");
-        // $stmt->bindParam();
-        $stmt->execute([$name, $age]);
-
-        if (!$stmt) {
-            die($conn->error);
-        }
-
-        $stmt = NULL;
-    }
-?>
+                foreach( $tests as $t ) {
+                    echo "<tr>";
+                    echo "<td>" . $t['tid'] . "</td>";
+                    echo "<td>" . $t['name'] . "</td>";
+                    echo "<td>" . $t['age'] . "</td>";
+                    echo "</tr>";
+                }
+            }
+        ?>
+        </tbody>
+    </table>
+</body>
+</html>
