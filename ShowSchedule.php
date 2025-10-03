cb974b3c1dc87581bc4898cf73869ced6f3f9652
StevenChinMaho <503steven10@gmail.com>
2025-10-03 10:34:52 +0800

    feat: First Schedule Query

--- ShowSchedule.php
+++ ShowSchedule.php
@@ -5,6 +5,67 @@
 <html>
 <head lang='zh-TW'>
     <meta charset="UTF-8">
-
+    <title>課表查詢</title>
 </head>
+<body>
+    <?php
+    $class_id = $_GET['class_id'];
+
+    $stmt = $pdo->prepare("SELECT class_name FROM class WHERE class_id = ?");
+    $stmt->execute([$class_id]);
+    $class_name = $stmt->fetchColumn();
+
+    echo "<h1>" . $class_name . " 的課表</h1><br>";
+    ?>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>節次</th>
+                    <th>一</th>
+                    <th>二</th>
+                    <th>三</th>
+                    <th>四</th>
+                    <th>五</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- <tr>
+                <td>1</td>
+                    <td><div>數學</div><div>金怡安</div></td>
+                    <td><div>數學</div><div>金怡安</div></td>
+                    <td><div>數學</div><div>金怡安</div></td>
+                    <td><div>數學</div><div>金怡安</div></td>
+                    <td><div>數學</div><div>金怡安</div></td>
+                </tr> -->
+                <?php
+                    $stmt = $pdo->prepare("SELECT 
+                        sub.subject_name,
+                        tea.teacher_name
+                    FROM schedule s
+                    JOIN timeslot tim ON s.timeslot_id = tim.timeslot_id
+                    JOIN subject sub ON s.subject_id = sub.subject_id
+                    JOIN teacher tea ON s.teacher_id = tea.teacher_id
+                    WHERE s.class_id = ? && tim.period = ?
+                    ORDER BY tim.weekday");
+                    $stmt->execute([$class_id, 1]);
+                    
+                    for( $i = 1; $i <= 8; $i++ )
+                    {
+                        echo "<tr>";
+                        echo "<td>$i</td>";
+                        $stmt->execute([$class_id, $i]);
+                        $row = $stmt->fetchAll(); //array(5) { [0]=> array(2) { ["subject_name"]=> string(6) "國文" ["teacher_name"]=> string(9) "黃佩筠" } [1]=> array(2) { ["subject_name"]=> string(6) "家政" ["teacher_name"]=> string(9) "陳璟賢" } [2]=> array(2) { ["subject_name"]=> string(6) "體育" ["teacher_name"]=> string(9) "梁景彥" } [3]=> array(2) { ["subject_name"]=> string(6) "國文" ["teacher_name"]=> string(9) "黃佩筠" } [4]=> array(2) { ["subject_name"]=> string(12) "生活科技" ["teacher_name"]=> string(9) "黃詩淳" } }
+
+                        foreach( $row as $c ) 
+                        {
+                            echo "<td><div>" . $c['subject_name'] . "</div><div>" . $c['teacher_name'] . "</div></td>";
+                        }
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
 </html>
--- index.php
+++ index.php
@@ -17,11 +17,32 @@
             <br>
         </p>
         <hr>
-        <form method="post" action="sendForm.php">
-            name: <input type="text" name="name"/><br>
-            age: <input type="text" name="age"/><br>
-            <input type="submit">
-        </form>
+        <div>
+            <form method="post" action="sendForm.php">
+                name: <input type="text" name="name"/><br>
+                age: <input type="text" name="age"/><br>
+                <input type="submit">
+            </form>
+            <hr>
+            <p>選擇要查詢的班級：</p>
+            <form method="get" action="ShowSchedule.php">
+                <select name="class_id">
+                    <option value="1">七年一班</option>
+                    <option value="2">七年二班</option>
+                    <option value="3">七年三班</option>
+                    <option value="4">七年四班</option>
+                    <option value="5">八年一班</option>
+                    <option value="6">八年二班</option>
+                    <option value="7">八年三班</option>
+                    <option value="8">八年四班</option>
+                    <option value="9">九年一班</option>
+                    <option value="10">九年二班</option>
+                    <option value="11">九年三班</option>
+                    <option value="12">九年四班</option>
+                </select>
+                <input type="submit">
+            </form>
+        </div>
         <hr>
         <table>
             <thead>
