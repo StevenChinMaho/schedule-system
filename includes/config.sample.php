c8f03130cca941d3a97fd5f34f63dabf4057029f
StevenChinMaho <503steven10@gmail.com>
2025-10-19 16:32:47 +0800

    feat: enforce native prepares and utf8mb4 charset

--- includes/config.sample.php
+++ includes/config.sample.php
@@ -5,15 +5,19 @@ define('DB_PASS', '');
 define('DB_NAME', 'the_DB');
 
 try {
-    $pdo = new PDO( "mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASS );
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
+    $pdo = new PDO( 
+        "mysql:host=" . DB_HOST . "; dbname=" . DB_NAME . ";charset=utf8mb4", 
+        DB_USER, 
+        DB_PASS,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+        ]
+    );
 } catch ( PDOException $e ) {
     error_log("資料庫連線失敗: " . $e->getMessage());
-    
     http_response_code(503);
     die('
     <!DOCTYPE html>
