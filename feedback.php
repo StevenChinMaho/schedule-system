2a3349ff085ab4c9e756d3309432d6be675c9ad4
StevenChinMaho <503steven10@gmail.com>
2025-10-20 18:04:58 +0800

    fix: amend titles in information and feedback pages

--- feedback.php
+++ feedback.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>湖內國中調課查詢系統 - 首頁</title>
+    <title>湖內國中調課查詢系統 - 意見回饋</title>
     <link rel="stylesheet" href="css/frame.css">
 </head>
 <body>
